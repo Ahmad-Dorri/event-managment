d596b2ba19c15f5f34148eb510612d1fc16411a8
= <=>
2024-07-28 08:09:10 +0330

    Add resources for use and event

--- source/app/Http/Controllers/Api/EventController.php
+++ source/app/Http/Controllers/Api/EventController.php
@@ -4,49 +4,52 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EventRequest;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class EventController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): ResourceCollection
     {
-        return Event::query()->latest()->paginate(25);
+        return EventResource::collection(Event::query()->latest()->paginate(25));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(EventRequest $request)
+    public function store(EventRequest $request): EventResource
     {
         $data = $request->validated();
-        return Event::query()->create([...$data, 'user_id' => 1]);
+        return new EventResource(Event::query()->create([...$data, 'user_id' => 1]));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Event $event)
+    public function show(Event $event): EventResource
     {
-        return $event;
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(EventRequest $request, Event $event)
+    public function update(EventRequest $request, Event $event): EventResource
     {
         $data = $request->validated();
         $event->update($data);
-        return $event;
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Event $event)
+    public function destroy(Event $event): JsonResponse
     {
         $event->delete();
         return response()->json(status: 204);
